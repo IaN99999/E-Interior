7f283c87324891cf915e2022e1b13aee16be4dfe
Andre Leo <leocristian581@gmail.com>
2025-07-22 09:40:39 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,9 @@
     <meta charset="UTF-8">
     <title>@yield('title', 'MySite')</title>
     @vite('resources/css/app.css')
+    <!-- Bootstrap CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
     @livewireStyles
 </head>
 
@@ -18,7 +21,6 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-   
     @livewireScripts
 </body>
 
